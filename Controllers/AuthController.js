e89b587ac695b5551c1e5c8f7990ce5d963bf758
mafewo <mwolters@maxisistemas.com.ar>
2018-04-07 14:36:27 -0300

    call to method of class from router method

---
+++ Controllers/AuthController.js
@@ -0,0 +1,47 @@
+import { createJWToken } from '../libs/auth'
+  
+class AuthController {
+  constructor(request, response) {
+    this.request = request
+    this.response = response
+  }
+
+  login (request) {
+    let { email, password } = this.request.body
+    let token = createJWToken({
+      sessionData: {
+        email: email,
+        password: password
+      },
+      maxAge: 3600
+    })
+    this.response.status(200)
+      .json({
+        success: true,
+        token: token
+      })
+    /*
+      db.User.findByEmail(email)
+        .then((user) => (!user) ? Promise.reject("User not found.") : user)
+        .then((user) => user.comparePassword(password))
+        .then((user) => user.publicParse(user))
+        .then((user) => {
+          this.response.status(200)
+            .json({
+              success: true,
+              token: createJWToken({
+                sessionData: user,
+                maxAge: 3600
+              })
+            })
+        })
+        .catch((err) => {
+          this.response.status(401)
+            .json({
+              message: err || "Validation failed. Given email and password aren't matching."
+            })
+        }) */
+  }
+}
+var a = new AuthController();
+module.exports = {AuthController: AuthController};
--- routes/router.js
+++ routes/router.js
@@ -1,51 +1,18 @@
 'use strict'
-
 import express from 'express'
 import asyncify from 'express-asyncify'
 import auth from 'express-jwt'
 
 import { authentication } from '../config/config'
 import { ErrorHandler } from '../libs/ErrorHandler'
-import { createJWToken } from '../libs/auth'
-
+import { AuthController } from '../Controllers/AuthController'
+// const AuthController = require('../Controllers/AuthController').default
 const debug = require('debug')('api-rest:api:routes')
 const router = express.Router()
 
-router.post('/login', (req, res) => {
-  let { email, password } = req.body
-  let token = createJWToken({
-    sessionData: {
-      email: email,
-      password: password
-    },
-    maxAge: 3600
-  })
-  res.status(200)
-    .json({
-      success: true,
-      token: token
-    })
-  /*
-    db.User.findByEmail(email)
-      .then((user) => (!user) ? Promise.reject("User not found.") : user)
-      .then((user) => user.comparePassword(password))
-      .then((user) => user.publicParse(user))
-      .then((user) => {
-        res.status(200)
-          .json({
-            success: true,
-            token: createJWToken({
-              sessionData: user,
-              maxAge: 3600
-            })
-          })
-      })
-      .catch((err) => {
-        res.status(401)
-          .json({
-            message: err || "Validation failed. Given email and password aren't matching."
-          })
-      }) */
+router.post('/login', (req, res, next) => {
+  const authcontroller = new AuthController(req, res)
+  authcontroller.login()
 })
 
 router.get('/agents', auth(authentication), (req, res) => {
@@ -72,4 +39,4 @@ router.get('/metrics/:uuid/:type', (req, res) => {
   res.send({ uuid, type })
 })
 
-module.exports = router
+export default router;
